583aced99692e3e15170c0017f8c13edcfc2b4cd
cliffcantcode <cliffcantcode@gmail.com>
2022-10-21 21:59:59 -0400

    made query struct to store parsed values

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,56 @@
-use clap::{Parser, ValueEnum};
+use clap::{Parser, ValueEnum, Subcommand};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    #[arg(short, long, value_enum)]
-    year: YearMode,
+    #[command(subcommand)]
+    command: Commands,
 }
 
-#[derive(Clone, ValueEnum)]
+#[derive(Subcommand)]
+enum Commands {
+    /// scrapes lego sets by year made
+    Year { mode: Option<YearMode> },
+}
+
+#[derive(Clone, Ord, PartialOrd, Eq, PartialEq, ValueEnum)]
 enum YearMode {
     None,
     Current,
     All,
 }
 
+// TODO: add query for scraping
+struct Query {
+    years: Option<Vec<u16>>,
+}
+
+impl Query {
+    fn new() -> Self {
+        Query { years: None }
+    }
+
+    fn set_years(&mut self, years: Vec<u16>) {
+        let _ = self.years.take();
+        self.years = Some(years);
+    }
+}
+
 fn main() {
     let cli = Cli::parse();
 
-    match cli.year {
-        YearMode::None => println!("None"),
-        YearMode::Current => println!("{}", 2022),
-        YearMode::All => println!("All"),
+    match &cli.command {
+        Commands::Year { mode } => match mode {
+            Some(YearMode::None) => println!("None"),
+            Some(YearMode::Current) => println!("{}", 2022),
+            Some(YearMode::All) => println!("All"),
+            None => {
+                println!("{}", 2022); 
+            }
+        }
     }
+    
+    let mut query = Query::new();
+    query.set_years(vec![2022]);
+    println!("{:?}", query.years.unwrap()[0]);
 }
